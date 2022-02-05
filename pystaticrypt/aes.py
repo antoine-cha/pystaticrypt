bbf6ce443215ba057951e5a6d1bfd6db79ff5fee
antoine-cha <antoine.chassang0@gmail.com>
2022-02-05 10:35:29 +0000

    AES with salt and iv

--- pystaticrypt/aes.py
+++ pystaticrypt/aes.py
@@ -1,31 +1,40 @@
 import base64
-import hashlib
-from typing import AnyStr
 
-from Crypto import Random  # type: ignore
 from Crypto.Cipher import AES  # type: ignore
+from Crypto.Hash import SHA256  # type: ignore
+from Crypto.Protocol.KDF import PBKDF2  # type: ignore
+from Crypto.Random import get_random_bytes  # type: ignore
+from Crypto.Util.Padding import pad, unpad  # type: ignore
 
 
 class AESCipher(object):
-    def __init__(self, key):
+    def __init__(self, password: str):
+        self.password = password
         self.bs = AES.block_size
-        self.key = hashlib.sha256(key.encode()).digest()
+        self.salt_size = 16
+        self.iv_size = 16
 
-    def encrypt(self, raw: str) -> bytes:
-        raw = self._pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+    def _get_key(self, salt: bytes) -> bytes:
+        key = PBKDF2(self.password, salt, 32, count=1_0000, hmac_hash_module=SHA256)
+        return key
 
-    def decrypt(self, enc: bytes) -> str:
+    def _gen_salt(self) -> bytes:
+        return get_random_bytes(self.salt_size)
+
+    def _gen_iv(self) -> bytes:
+        return get_random_bytes(self.iv_size)
+
+    def encrypt(self, raw: bytes) -> bytes:
+        raw = pad(raw, self.bs, style='pkcs7')
+        salt = self._gen_salt()
+        iv = self._gen_iv()
+        cipher = AES.new(self._get_key(salt), AES.MODE_CBC, iv)
+        return base64.b64encode(salt + iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc: bytes) -> bytes:
         enc = base64.b64decode(enc)
-        iv = enc[: AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size :])).decode('utf-8')
-
-    def _pad(self, s: AnyStr) -> AnyStr:
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
-
-    @staticmethod
-    def _unpad(s: AnyStr) -> AnyStr:
-        return s[: -ord(s[len(s) - 1 :])]
+        salt = enc[: self.bs]
+        iv = enc[self.bs : 2 * self.bs]
+        cipher = AES.new(self._get_key(salt), AES.MODE_CBC, iv)
+        decrypted = cipher.decrypt(enc[2 * self.bs :])
+        return unpad(decrypted, self.bs, style='pkcs7')
--- tests/test_aes.py
+++ tests/test_aes.py
@@ -3,8 +3,7 @@ from pystaticrypt.aes import AESCipher
 
 def test_encrypt():
     cipher = AESCipher("key")
-    text = "my text is nice"
+    text = "my text is nice".encode('utf-8')
     encrypted = cipher.encrypt(text)
     decrypted = cipher.decrypt(encrypted)
-    assert isinstance(decrypted, str)
     assert text == decrypted
